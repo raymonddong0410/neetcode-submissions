45b184029559580a9ff123f182661472b55959da
Raymond Dong <116387749+raymonddong0410@users.noreply.github.com>
2026-09-15 20:47:41 -0500

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,64 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # brute would be a nested loop
+        # res = []
+        # for i in range(len(nums)):
+        #     product = 1
+        #     for j in range(len(nums)):
+        #         if i != j:
+        #             product *= nums[j]
+        #     res.append(product)
+        # return res
+
+        # better idea is prefix sum?
+        # calc before and after the val
+        # take the before multiply and so you know at that index the before will equal to 
+
+        # would have to find the array before and after
+        # length = len(nums)
+        # pre = [0] * length
+        # post = [0] * length
+        # res = []
+
+        # pre[0] = 1
+        # post[length - 1] = 1
+
+        # for i in range(1,length):
+        #     # can't get pre starting from index one cause no index before
+        #     # nums --> 1, 2, 3....
+        #     # pre --> 1, 1, 2....
+        #     # pre is delayed because we want to store val of prod of prev val at that index
+        #     pre[i] = nums[i - 1] * pre[i - 1]
+        
+        # for i in range(length-2,-1,-1):
+        #     post[i] = nums[i + 1] * post[i + 1]
+        
+        # # for the result we want to multiple pre and post at that point
+        # # 0 accounted for --> we multiply by first and last vals in list
+        # for i in range(length):
+        #     # can also make this faster by pre populating res. replacing val is constant like append is amortoried o 1
+        #     res.append(pre[i] * post[i])
+        
+        # return res
+
+        # can also do prefix sum in place. same idea but use one res array
+        x = len(nums)
+        res = [0] * x
+
+        pre = 1
+        for i in range(x):
+            # what this does is build the prefix in place and if nums[0] is 0 then everything ends up in res as 0 
+            # same idea as above but not using an array, instead a constant
+            res[i] = pre
+            pre *= nums[i]
+        
+        post = 1
+        for i in range(x-1, -1, -1):
+            # at this point you know last elem will just be what the pre prod will be cause there's nothing after
+            # other spots multiple the post by pre prod already calced and held in the res
+            res[i] *= post
+            post *= nums[i]
+
+        return res
+
+        
